b9adef9539f6232465687c7b42bfa92b6743e396
Kooksun <leeksnet@altimedia.com>
2025-12-09 16:22:22 +0900

    안쓰는 파일 삭제

--- check_models.py
+++
@@ -1,9 +0,0 @@
-
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyBhuHhc4MHSVNlazZCQ44kibmzjpW-M0e0")
-
-print("Listing available models:")
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(m.name)
